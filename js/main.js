6655f48a3b41f4e220187a015c5dfd73e62e8660
matsjp <smuckenduck@gmail.com>
2016-10-19 11:19:16 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,25 +1,15 @@
-function include() 
-{
-  var allElements = document.getElementsByTagName("*");
-  for (i = 0; i < allElements.length; i++) 
-	{
-    if (allElements[i].getAttribute("include")) 
-		{
-      
-      var fileName = allElements[i].getAttribute("include");
-      var request = new XMLHttpRequest();
-      request.onreadystatechange = function() 
-			{
-        if (this.readyState == 4 && this.status == 200) 
-				{
-          allElements[i].removeAttribute("include");
-          allElements[i].innerHTML = this.responseText;
-          include();
-        }
-      }      
-      request.open("GET", fileName, true);
-      request.send();
-      return;
-    }
-  }
+//The following function takes a filepath as parameter and the content of the file is then added inside the body tag
+//The place where the content is added is wherever this script was called in the document flow
+//To use this function to get content to a html file, you write:
+//<script>loadFromDoc(filepath)</script>
+function loadFromDoc(filePath){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+			var body = document.getElementsByTagName("body");
+			body[0].innerHTML += this.responseText;
+		}
+	}
+	xhttp.open("GET", filePath);
+	xhttp.send();
 }
